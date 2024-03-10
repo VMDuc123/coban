ce06f6e9c5b1e7eac50c5245dc0a88b10362e453
VuDuc <vuduc7123@gmail.com>
2024-03-10 17:30:30 +0700

    hoc

---
+++ Buoi5/Buoi5.js
@@ -0,0 +1,4 @@
+let a = 15;
+let b = 6; 
+let c = 2003;
+console.log(`Vu Minh Duc sinh ngay ${a} thang ${b} nam ${c}`.split("",3))
---
+++ buoi10/buoi10.js
@@ -0,0 +1,16 @@
+alert('vu minh duc');
+console.log('vu minh duc')
+let a = "vu minh duc"
+let b = {name:'duc',
+        tuoi: "20",
+        sex: 'nam',
+        jod: 'student'   };
+let c=[{name:'van',tuoi:'20'},
+        {name:'uyen',tuoi:'20'}];
+let e = 'name'
+b[e]='van'
+console.log('thuoc tinh',b,'kieu cua b',typeof(b));
+console.log('ten toi la:',b.name, 'tuoi:',b['tuoi'])
+console.log('thuoc tinh',c,'kieu cua c',typeof c)
+console.log(c[1]);
+
